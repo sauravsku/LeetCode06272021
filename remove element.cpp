b5e574bd167ce78a608e80558b8882f58c3c27cb
sauravsku <86544380+sauravsku@users.noreply.github.com>
2021-11-02 09:43:39 +0530

    Delete remove element.cpp

--- remove element.cpp
+++
@@ -1,7 +0,0 @@
-class Solution {
-public:
-    int removeElement(vector<int>& nums, int val) {
-        nums.erase(std::remove(nums.begin(),nums.end(),val),nums.end());
-        return nums.size();
-    }
-};
